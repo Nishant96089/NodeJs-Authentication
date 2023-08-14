7177447dd7532e1a8e81b04684202240cba80039
Nishant Sinha <nishantsinha.hulk@gmail.com>
2023-08-14 11:32:58 +0530

    nodejs authentication

---
+++ config/key.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MongoURI: "mongodb+srv://poojapanwar:poojapanwarxyz@cluster0.hha0ca6.mongodb.net/?retryWrites=true&w=majority"
+<<<<<<< HEAD
+}
+=======
+    }
+>>>>>>> 76d697d0714fff90d81b2c941218e7d60e3b4968
